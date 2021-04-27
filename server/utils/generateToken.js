405ffc1af8ea238d534de4c2fec5fd16d7263b42
Dan <dan.bayford@gmail.com>
2021-04-27 17:34:38 +0100

    feat: moved config object into axios requests due to token issue

--- server/utils/generateToken.js
+++
@@ -1,8 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-//User variable to use in token, JWT secret and options object
-export const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: '30d'      //30d === 30 days
-  })
-}
